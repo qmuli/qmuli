546f0f888f6ef5815bfcef8ebf9b661497ae408f
Alex Babkin <ababkin@gmail.com>
2016-12-31 00:18:58 -0600

    add ddb tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -10,6 +10,8 @@ import           Test.Tasty.Hspec
 
 import qualified Config.ApiGw.Simple              as ApiGw.Simple
 import qualified Config.ApiGw.UserPoolsAuthorizer as ApiGw.UserPoolsAuthorizer
+import qualified Config.DDB.Simple                as DDB.Simple
+import qualified Config.DDB.Stream                as DDB.Stream
 import qualified Config.Identifier                as Id
 import qualified Config.Template                  as Template
 
@@ -24,6 +26,8 @@ main = do
       , testSpec "Identifier"  Id.spec
       , testSpec "ApiGw Simple"               ApiGw.Simple.spec
       , testSpec "ApiGw UserPools Authorizer" ApiGw.UserPoolsAuthorizer.spec
+      , testSpec "DDB Simple"               DDB.Simple.spec
+      , testSpec "DDB Stream"               DDB.Stream.spec
       ]
 
 {- decode json `shouldBe` Just createEvent -}
