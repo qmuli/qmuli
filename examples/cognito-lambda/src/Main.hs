b2048734c4b0e09d47487cce016c9ae25d44e5df
Alex Babkin <ababkin@gmail.com>
2017-01-08 10:01:35 -0500

    wip cognito lambda example

--- examples/cognito-lambda/src/Main.hs
+++ examples/cognito-lambda/src/Main.hs
@@ -1,37 +1,137 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import           Control.Lens                hiding (view, (.=))
-import           Control.Monad               (void)
-import           Data.Aeson                  (Value (String))
-import           Data.Default                (def)
-import qualified Data.HashMap.Strict         as SHM
-import qualified Data.Text                   as T
-import           Web.JWT                     (claims, decode)
+import           Control.Lens                    hiding (view, (.=))
+import           Control.Monad                   (forM, void)
+import           Data.Aeson
+import           Data.Aeson.Lens
+import           Data.Default                    (def)
+import           Data.Hashable                   (Hashable, hash)
+import qualified Data.HashMap.Strict             as SHM
+import           Data.Text                       (Text)
+import qualified Data.Text                       as T
+import           Network.AWS.DynamoDB.DeleteItem
+import           Network.AWS.DynamoDB.GetItem
+import           Network.AWS.DynamoDB.PutItem
+import           Network.AWS.DynamoDB.Scan
+import           Web.JWT                         (claims, decode)
 
-import           Qi                          (withConfig)
-import           Qi.Program.Config.Interface (ConfigProgram, customResource,
-                                              genericLambda)
-import           Qi.Program.Lambda.Interface (GenericLambdaProgram, say)
-import           Qi.Util                     (argumentsError, success)
-import           Qi.Util.Cognito             (cognitoPoolProviderLambda)
+import           Qi                              (withConfig)
+import           Qi.Config.AWS.DDB               (DdbAttrDef (..),
+                                                  DdbAttrType (..))
+import           Qi.Config.Identifier            (DdbTableId)
+import           Qi.Program.Config.Interface     (ConfigProgram, customResource,
+                                                  ddbTable, genericLambda)
+import           Qi.Program.Lambda.Interface     (CompleteLambdaProgram,
+                                                  GenericLambdaProgram,
+                                                  deleteDdbRecord, getDdbRecord,
+                                                  putDdbRecord, say,
+                                                  scanDdbRecords)
+import           Qi.Util
+import           Qi.Util.Cognito                 (cognitoPoolProviderLambda)
+import           Qi.Util.DDB
+
+import           Types
+import           Types.Contact
+
 
 main :: IO ()
 main = withConfig config
   where
     config :: ConfigProgram ()
     config = do
-      cognito <- customResource "cognitoPoolProvider" cognitoPoolProviderLambda def
-      void $ genericLambda "greet" greetLambda def
+      cognito <- customResource "cognitoPoolProvider" cognitoPoolProviderLambda $ def
+
+      contactsTable <- ddbTable "contacts" (DdbAttrDef "Id" S) def
+
+      genericLambda "scanContacts"  (scanContacts contactsTable) def
+      genericLambda "postContact"   (postContact contactsTable) def
+      genericLambda "getContact"    (getContact contactsTable) def
+      genericLambda "putContact"    (putContact contactsTable) def
+      genericLambda "deleteContact" (deleteContact contactsTable) def
+
+      {- genericLambda "getContactLogs" (getContactLogs contactsTable) def -}
+      {- genericLambda "putContactLog"  (putContactLog contactsTable) def -}
+
+      return ()
 
 
-    greetLambda
-      :: GenericLambdaProgram
-    greetLambda event = do
-      say "greetings!"
-      success "lambda had executed successfully"
+
+scanContacts
+  :: DdbTableId
+  -> GenericLambdaProgram
+scanContacts ddbTableId payload = do
+  r <- scanDdbRecords ddbTableId
+  withSuccess (r^.srsResponseStatus) $
+    result
+      (internalError . ("Parsing error: " ++))
+      (success . (toJSON :: [Contact] -> Value))
+      $ forM (r^.srsItems) parseAttrs
+
+postContact
+  :: DdbTableId
+  -> GenericLambdaProgram
+postContact ddbTableId payload =
+  withDeserializedPayload payload $ \(contact :: Contact) -> do
+    r <- putDdbRecord ddbTableId . toAttrs $ addId contact
+    withSuccess (r^.pirsResponseStatus) $
+      success "successfully posted contact"
+  where
+    addId :: Contact -> Contact
+    addId c = c{cId = T.pack . show $ hash c}
+
+getContact
+  :: DdbTableId
+  -> GenericLambdaProgram
+getContact ddbTableId payload =
+  withId payload $ \cid -> do
+    r <- getDdbRecord ddbTableId $ idKeys cid
+    withSuccess (r^.girsResponseStatus) $
+      result
+        (internalError . ("Parsing error: " ++))
+        (success . (toJSON :: Contact -> Value))
+        $ parseAttrs $ r^.girsItem
 
 
+putContact
+  :: DdbTableId
+  -> GenericLambdaProgram
+putContact ddbTableId payload =
+  withDeserializedPayload payload $ \(contact :: Contact) -> do
+    r <- putDdbRecord ddbTableId $ toAttrs contact
+    withSuccess (r^.pirsResponseStatus) $
+      success "successfully put contact"
 
+deleteContact
+  :: DdbTableId
+  -> GenericLambdaProgram
+deleteContact ddbTableId payload =
+  withId payload $ \cid -> do
+      r <- deleteDdbRecord ddbTableId $ idKeys cid
+      withSuccess (r^.dirsResponseStatus) $
+        success "successfully deleted contact"
+
+
+withDeserializedPayload
+  :: FromJSON a
+  => Value
+  -> (a -> CompleteLambdaProgram)
+  -> CompleteLambdaProgram
+withDeserializedPayload payload f =
+  result
+    (internalError . ("Error: fromJson: " ++))
+    f
+    $ fromJSON payload
+
+withId
+  :: Value
+  -> (Text -> CompleteLambdaProgram)
+  -> CompleteLambdaProgram
+withId payload f =
+  maybe
+    (argumentsError "payload is missing the 'Id' attribute")
+    f
+    $ payload ^? key "Id" . _String
 
