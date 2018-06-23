b064cff817e89367f78cfd6140d709f18961dc72
Alex Babkin <ababkin@gmail.com>
2018-06-23 15:12:57 -0400

    fix payload passing in js shim

--- js/index.js
+++ js/index.js
@@ -12,29 +12,39 @@ exports.handler = (event, context, callback) => {
   const appName = prefixedLambdaName.split('_', 1);
   const lbdName = prefixedLambdaName.split('_').slice(1);
   const payload = JSON.stringify(event)
-    .replace(/\\/g, "\\\\")
-    .replace(/\$/g, "\\$")
-    .replace(/'/g, "\\'")
-    .replace(/"/g, "\\\"");
+    .replace(/\r\n/g, "")
+    .replace(/\n/g, "") + "\n"
+    // .replace(/\\/g, "\\\\")
+    // .replace(/\$/g, "\\$")
+    // .replace(/'/g, "\\'")
+    // .replace(/"/g, "\\\"");
 
   console.log('lambda payload: \"', payload, '\"')
 
   // the executable built in the AWS environment using Docker will always
   // be named 'lambda'
-  var child = spawn('./lambda', [appName, 'lbd', 'execute', '--lambda-name', lbdName, '--lambda-payload', payload]);
-  child.stdout.on('data', out => {
-    out.toString().split("\n").map(line => {
+  var child = spawn('./lambda', [ appName, 'lbd', 'execute', '--lambda-name', lbdName ]);
+
+  child.stdin.setEncoding('utf-8');
+
+  child.stdout.on('data', data => {
+    data.toString().split("\n").map(line => {
       if (line != "") // sometimes we get transient spurious empty lines from this node.js stdout event mechanism
         console.log(line);
     });
   });
 
-  child.stderr.on('data', out => {
-    console.log(out.toString());
+  child.stderr.on('data', data => {
+    console.log(data.toString());
   });
-  child.on('exit', exitCode => {
-    callback(null, exitCode);
+
+
+  child.on('exit', output => {
+    callback(null, output);
   });
+
+  child.stdin.write( payload );
+  child.stdin.end();
 
 }
 
